7fc62b14f4a1ae1101bec898cfc3c486dad64c81
Darren <d-pierre90@hotmail.com>
2020-02-05 10:28:31 -0500

    Added new webscraper for 4106 class that will be used in a cron job

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# DocumentCrawler
+# Document Crawler
+
+Document crawler is a web scraper that is used to download documents from websites.It can be used for automation of downloading resources that are posted regularly from a site that you want later want to check out.
+## Motivation
+The motivation of this project was that I sometimes come across a website that has many interesting documents either in pdf or PowerPoint I want to get. But there's a lot and I don't want to waste time going one by one and downloading it all . Why not web scrape them. This was also an excuse to experiment with python Beautiful Soup library to see what it can do , experience web scraping first hand and find it's limitations.This code often serves as my basis for web scraping certain web sites.
+
+## Installation
+
+clone the repo and run the following command
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+Copy main.py into a new file .Then edit the new file to meets your needs.You probably want to implement a filter function so that you exclude the links that you don't want . Also you want to insert the link or links that you want to scrape . Document Scrapter assumes that the links that you want are on the HTML page you've initially hand into the script.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- personalizedScripts/4106Scraper.py
+++ personalizedScripts/4106Scraper.py
@@ -33,7 +33,7 @@ def getURLContent(url):
         log.error(f"Unable to get content of the following URL:{url}.The requests return a status code of {response.status_code}")
         
 
-def downloadLink(url_source,url,destination="./4107/"):
+def downloadLink(url_source,url,destination="./"):
     log.info(f"Attempting to download:{url}")
     if not('http' in url):
        url=url_source+url
@@ -47,21 +47,35 @@ def downloadLink(url_source,url,destination="./4107/"):
 
     else:
         log.info("{url} is not a downloadable pdf")
+
+def isLectureNote(link):
+    # Filter links that are only the notes
+    noteString="AI"
+    noteString2="Ch"
+    if noteString in link and noteString2 in link.get('href'):
+        return True
+
+    else:
+        return False
+
 def main():
 
-    url= 'https://sikaman.dyndns.org:8443/WebSite/rest/site/courses/4107//assignments/'
+    url= 'https://people.scs.carleton.ca/~oommen/Courses/COMP4106Winter20/'
     urlContent= getURLContent(url)
 
     if(urlContent):
         PARSER=BeautifulSoup(urlContent, 'html.parser')
         info=f""
         links=PARSER.find_all('a',href=True)
+        links=filter(isLectureNote,links)
         log.info(f"The following url:{url} has the following links:")
         for link in links:
             log.info(link.get('href'))
 
+        
+        destination="/home/darren/CU/4106/Lectures/"
         for link in links:
-            downloadLink(url,link.get('href'))
+            downloadLink(url,link.get('href'),destination)
         
 
 
